7a5d85b33ed2e3555e047a5b1c50577630328ead
Francesco Occhipinti <focchipinti@gmail.com>
2012-02-08 13:16:16 +0100

    closes issue #1

--- setup_requisites.py
+++ setup_requisites.py
@@ -1,16 +1,25 @@
 import re
-def convert_req(req):
+import random
+
+def convert_req(req, use_random=True):
     '''
-    >>> r = convert_req('svn+svn://svn.myserver.com/repos/myproject_mail/')
+    >>> req = 'svn+svn://svn.myserver.com/repos/myproject_mail/'
+    >>> r = convert_req(req)
     >>> len(r)
     2
+    >>> r['install_requires'] #doctest: +ELLIPSIS
+    '...'
+    >>> r['dependency_links'] #doctest: +ELLIPSIS
+    'svn+svn://svn.myserver.com/repos/myproject_mail/#egg=...-dev'
+    >>> r = convert_req(req, use_random=False)
     >>> r['install_requires']
     'svnsvnsvnmyservercomreposmyproject_mail'
     >>> r['dependency_links']
     'svn+svn://svn.myserver.com/repos/myproject_mail/#egg=svnsvnsvnmyservercomreposmyproject_mail-dev'
     '''
     r = {}
-    fake_package_name = re.sub('\W', '', req)
+    if use_random: fake_package_name = str(random.randrange(1000000))
+    else:          fake_package_name = re.sub('\W', '', req)
     r['install_requires'] = fake_package_name
     r['dependency_links'] = '{0}#egg={1}-dev'.format(req, fake_package_name)
     return r
@@ -23,10 +32,10 @@ def convert(reqs):
     ... git+https://github.com/mozes/smpp.pdu.git
     ... """
     >>> reqs = convert(reqs)
-    >>> reqs['install_requires']
-    ['svnhttpssvnmyservercomreposmyproject', 'svnhttpssvnmyservercomreposmyproject_mail', 'githttpsgithubcommozessmpppdugit']
-    >>> reqs['dependency_links']
-    ['svn+https://svn.myserver.com/repos/myproject/#egg=svnhttpssvnmyservercomreposmyproject-dev', 'svn+https://svn.myserver.com/repos/myproject_mail/#egg=svnhttpssvnmyservercomreposmyproject_mail-dev', 'git+https://github.com/mozes/smpp.pdu.git#egg=githttpsgithubcommozessmpppdugit-dev']
+    >>> reqs['install_requires'] #doctest: +ELLIPSIS
+    ['...', '...', '...']
+    >>> reqs['dependency_links'] #doctest: +ELLIPSIS
+    ['svn+https://svn.myserver.com/repos/myproject/#egg=...-dev', 'svn+https://svn.myserver.com/repos/myproject_mail/#egg=...-dev', 'git+https://github.com/mozes/smpp.pdu.git#egg=...-dev']
     '''
     reqs_list  = reqs.split('\n')
     reqs_pairs = [convert_req(r) for r in reqs_list if r!='']
